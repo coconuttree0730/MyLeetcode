ee10956c69745f21e66a2876848f4a74a82ba18d
wuzhongpeng <wu.zhongpeng@foxmail.com>
2023-08-15 22:08:16 +0800

    第一次提交

---
+++ 数组_实现十进制转换其他进制.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define M 100
+static void zhuanhuan(int num_D,int num);
+int main(void)
+{
+
+    int num_D;
+    int num;
+    printf("请输入十进制：");
+    scanf("%d",&num_D);
+    printf("请输入待转换进制：");
+    scanf("%d",&num);
+    zhuanhuan(num_D,num);
+
+    exit(0);
+}
+
+
+static void zhuanhuan(int num_D,int num)
+{
+    int n[M];
+    int i=0;
+    do{
+        n[i]=num_D%num;
+        num_D=num_D/num;
+        i++;
+    }while (num_D!=0);
+    for (i--; i>=0; i--)
+    {
+        printf("%d",n[i]);
+    }
+    
+    printf("\n");
+    return ;
+}
